dbe56d68d459a31649275e06181aaa044d258932
正木拓馬 <tb.masaki0531@gmail.com>
2020-04-22 20:41:33 +0300

    initial commit

---
+++ src/assets/game-info.js
@@ -0,0 +1,53 @@
+export default {
+  players: ['player1', 'player2', 'player3', 'player4'],
+  cardsNum: {
+    12: 12,
+    16: 16,
+    20: 20,
+  },
+  cardChar: {
+    cat: [
+      './images/cat1.jpg',
+      './images/cat2.jpg',
+      './images/cat3.jpg',
+      './images/cat4.jpg',
+      './images/cat5.jpg',
+      './images/cat6.jpg',
+      './images/cat7.jpg',
+      './images/cat8.jpg',
+      './images/cat9.jpg',
+      './images/cat10.jpg',
+    ],
+    dog: [
+      './images/dog1.jpg',
+      './images/dog2.jpg',
+      './images/dog3.jpg',
+      './images/dog4.jpg',
+      './images/dog5.jpg',
+      './images/dog6.jpg',
+      './images/dog7.jpg',
+      './images/dog8.jpg',
+      './images/dog9.jpg',
+      './images/dog10.jpg',
+    ],
+    dinosour: [
+      './images/dino1.jpg',
+      './images/dino2.jpg',
+      './images/dino3.jpg',
+      './images/dino4.jpg',
+      './images/dino5.jpg',
+      './images/dino6.jpg',
+      './images/dino7.jpg',
+      './images/dino8.jpg',
+      './images/dummypic.jpg',
+      './images/dummypic.jpg',
+    ],
+  backImg: './images/backpic.jpg',
+  },
+
+};
+
+// the number of players
+// cards type ... cat dog dinosour
+// the number of cards
+// back image of photo
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// import gameInfo from '../assets/game-info';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+  },
+  getters: {
   },
   mutations: {
   },
