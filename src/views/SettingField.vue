1070e600ac2a1736703064704faf605422a153b4
正木拓馬 <tb.masaki0531@gmail.com>
2020-05-03 03:37:13 +0300

    revise responsive

--- src/views/SettingField.vue
+++ src/views/SettingField.vue
@@ -5,28 +5,28 @@
       <div class="players-select">
         <label for="players-select">Choose how many players:</label>
         <select name="players" id="players-select" v-model="nPlayer">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
+          <option value="1">1 </option>
+          <option value="2">2 </option>
+          <option value="3">3 </option>
+          <option value="4">4 </option>
         </select><br>
       </div>
 
       <div class="card-type-select">
         <label for="card-type-select">Choose which picture do you prefer:</label>
         <select name="card-type" id="card-type-select" v-model="cardChar">
-          <option value="cat">cat</option>
-          <option value="dog">dog</option>
-          <option value="dinosaur">dinosaur</option>
+          <option value="cat">cat </option>
+          <option value="dog">dog </option>
+          <option value="dinosaur">dinosaur </option>
         </select><br>
       </div>
 
       <div class="cards-select">
         <label for="cards-select">Choose how many cards:</label>
         <select name="cards" id="cards-select" v-model="nCard">
-          <option value="12">12</option>
-          <option value="16">16</option>
-          <option value="20">20</option>
+          <option value="12">12 </option>
+          <option value="16">16 </option>
+          <option value="20">20 </option>
         </select><br>
       </div>
 
@@ -71,7 +71,7 @@ export default {
   text-align: center;
   padding-top: 2vw;
   width: 70vw;
-  height:35vw;
+  height: 35vw;
   background: #0e58108c;
   border-radius: 10px;
   box-shadow: 0.7px 0.5px 0.5px 0px black;
@@ -113,6 +113,7 @@ label {
   .setting-option {
     font-size: 3vw;
     margin-top: 14vw;
+    height: 40vw;
   }
 
   #players-select, #card-type-select, #cards-select {
